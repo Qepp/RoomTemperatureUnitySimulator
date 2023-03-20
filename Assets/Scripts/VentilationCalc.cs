66856b861a113fc53cac17106dad7a9a551da369
Qepp <vaino@heinot.com>
2023-03-20 22:52:27 +0200

    New version of ventilation

--- Assets/Scripts/VentilationCalc.cs
+++ Assets/Scripts/VentilationCalc.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class VentilationCalc : MonoBehaviour
 {
+    public Temperature temperature, outsideTemp;
+    public float heatRecoveryRate;
+
+    public float acPowerUsage;
+
+    public float acPowerEfficiency;
+
+    public bool acOn;
+
+
+    [SerializeField]
+    private float ach; // air changes per hour
+
+    private float shc = 1010; //specific heat capacity J/(K*kg)
+    private double tempDiff;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        tempDiff = temperature.temperature - outsideTemp.temperature;
+        Ventilation();
+    }
+
+
+
+    private void Ventilation()
+    {
+        tempDiff -= tempDiff * heatRecoveryRate;
+        float volume = temperature.length * temperature.width * temperature.height;
+        double power = volume * tempDiff * ach * 0.33;
+        if (acOn)
+        {
+            power -= acPowerEfficiency * acPowerUsage;
+            Variables.Instance.WattsSpend(acPowerUsage);
+        }
+        temperature.temperature -= (power * Time.deltaTime * Variables.Instance.timeScale) / (shc * temperature.mass);
     }
 }
