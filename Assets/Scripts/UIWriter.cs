13e98b7619063249fcb99b014ce24d269d634d4a
Qepp <vaino@heinot.com>
2023-03-23 18:10:57 +0200

    Modified ui

--- Assets/Scripts/UIWriter.cs
+++ Assets/Scripts/UIWriter.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 
 public class UIWriter : MonoBehaviour
 {
-    public Temperature writtenTemperature;
+    public Temperature writtenTemperature1;
     public Temperature outsideTemperature;
     [SerializeField]
     private TextMeshProUGUI outsideTempText;
     [SerializeField]
-    private TextMeshProUGUI insideTempText;
+    private TextMeshProUGUI insideTemp1Text;
+    public Temperature writtenTemperature2;
+    [SerializeField]
+    private TextMeshProUGUI insideTemp2Text;
+    public Temperature writtenTemperature3;
+    [SerializeField]
+    private TextMeshProUGUI insideTemp3Text;
     // Start is called before the first frame update
     void Start()
     {
@@ -26,7 +32,9 @@ public class UIWriter : MonoBehaviour
     private void WriteTempUiCanvas()
     {
         outsideTempText.text = outsideTemperature.temperature.ToString("F2") + "°C";
-        insideTempText.text = writtenTemperature.temperature.ToString("F3") + "°C";
+        insideTemp1Text.text = writtenTemperature1.temperature.ToString("F3") + "°C";
+        insideTemp2Text.text = writtenTemperature2.temperature.ToString("F3") + "°C";
+        insideTemp3Text.text = writtenTemperature3.temperature.ToString("F3") + "°C";
     }
 
 
